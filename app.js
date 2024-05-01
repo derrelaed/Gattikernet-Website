1d9a80e84bf38185209d4900aafa8175b2f3e004
derrelaed <168674932+derrelaed@users.noreply.github.com>
2024-05-01 21:27:56 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            // Dynamische Daten können hier eingefügt werden
+        };
+    },
+    methods: {
+        submitForm() {
+            alert
