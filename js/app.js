2f244badbe006e6fb89f801d5a360f78b376f9d0
derrelaed <168674932+derrelaed@users.noreply.github.com>
2024-05-01 21:46:39 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            userName: '',
-            userEmail: '',
-            formSubmitted: false,
-            submissionMessage: 'Das Formular wurde erfolgreich gesendet!'
+            // Dynamische Daten können hier eingefügt werden
         };
     },
     methods: {
         submitForm() {
-            // Hier können zusätzliche Validierungen oder API-Aufrufe eingefügt werden.
-            this.formSubmitted = true;
-
-            // Setzen Sie die Formulardaten zurück oder führen Sie andere Aktionen aus
-            this.userName = '';
-            this.userEmail = '';
-        }
-    }
-});
-
-app.mount('#app');
+            alert
