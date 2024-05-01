4f477c264a6ed847a401e0c1095d95654b115816
derrelaed <168674932+derrelaed@users.noreply.github.com>
2024-05-01 21:28:09 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const app = Vue.createApp({
-    data() {
-        return {
-            // Dynamische Daten können hier eingefügt werden
-        };
-    },
-    methods: {
-        submitForm() {
-            alert
